e773bd95acf9aadd611a0e3f7860b37cb748f3b7
Vito <vslavilla@gmail.com>
2014-12-04 14:23:46 -0500

    stuff

---
+++ scripts/repeat.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('repeatApp', [])
+
+.controller('MainCtrl', function($scope) {
+  $scope.items = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o'
+  ];
+});
